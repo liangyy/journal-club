2cf9a6e88b7f0918f65831144eb626be61a8d9fb
Yanyu Liang <yanyul@uchicago.edu>
2020-10-05 20:41:58 -0500

    still on comments

--- themes/hugo-bootstrap-premium/static/js/github-comments.js
+++ themes/hugo-bootstrap-premium/static/js/github-comments.js
@@ -1,96 +1,85 @@
 // use of ajax vs getJSON for headers use to get markdown (body vs body_htmml)
 // todo: pages, configure issue url, open in new window?
 
-function ParseLinkHeader(link) {
-  if (link === null) {
-    return {};
-  }
+var CurrentPage = 0;
 
-  const entries = link.split(",");
-  const links = {};
-
-  for (const i in entries) {
-    const entry = entries[i];
-
-    const link = {};
-    link.name = entry.match(/rel=\"([^\"]*)/)[1];
-    link.url = entry.match(/<([^>]*)/)[1];
-    link.page = entry.match(/page=(\d+).*$/)[1];
-
-    links[link.name] = link;
-  }
-  return links;
+function ParseLinkHeader(link)
+{
+    if (! link)
+        return { };
+    var entries = link.split(",");
+    var links = { };
+    for (var i in entries)
+    {
+        var entry = entries[i];
+        var link = { };
+        link.name = entry.match(/rel=\"([^\"]*)/)[1];
+        link.url = entry.match(/<([^>]*)/)[1];
+        link.page = entry.match(/page=(\d+).*$/)[1];
+        links[link.name] = link;
+    }
+    return links;
 }
 
-function DoGithubComments(comment_id, page_id) {
-  const repo_name = "omegaphoenix/omegaphoenix.github.com";
+function DoGithubComments(comment_id, page_id)
+{
+    var repo_name = "liangyy/journal-club";
 
-  if (page_id === undefined) page_id = 1;
+    if (page_id === undefined)
+        page_id = 1;
 
-  const api_url = `https://api.github.com/repos/${repo_name}`;
-  const api_issue_url = `${api_url}/issues/${comment_id}`;
-  const api_comments_url = `${api_url}/issues/${comment_id}/comments?page=${page_id}`;
+    var url = "https://github.com/" + repo_name + "/issues/" + comment_id;
+    var api_url = "https://api.github.com/repos/" + repo_name;
+    var api_comments_url = api_url + "/issues/" + comment_id + "/comments" + "?page=" + page_id;
+    // var api_issue_url = api_url + "/issues/" + comment_id;
 
-  const url =
-    `https://github.com/${repo_name}/issues/${comment_id}`;
+    $(document).ready(function ()
+    {
+        // $.getJSON(api_issue_url, function(data) {
+        //     NbComments = data.comments;
+        // });
 
-  $(document).ready(function() {
-    $.getJSON(api_issue_url, function(data) {
-      NbComments = data.comments;
-    });
+        $.ajax(api_comments_url, {
+            headers: {Accept: "application/vnd.github.v3.html+json"},
+            dataType: "json",
+            success: function(comments, textStatus, jqXHR) {
 
-    $.ajax(api_comments_url, {
-      headers: { Accept: "application/vnd.github.v3.html+json" },
-      dataType: "json",
+                // Individual comments
+                $.each(comments, function(i, comment) {
 
-      success: function(comments, textStatus, jqXHR) {
-        // Add post button to first page
-        if (page_id == 1)
-          $("#gh-comments-list").append(
-            "<a href='" +
-              url +
-              "#new_comment_field' rel='nofollow' class='btn'>Post a comment on Github</a>"
-          );
+                    var date = new Date(comment.created_at);
 
-        // Individual comments
-        $.each(comments, function(i, comment) {
-          const date = new Date(comment.created_at);
+                    var t = "<div id='gh-comment'>";
+                    t += "<img src='" + comment.user.avatar_url + "' width='24px'>";
+                    t += "<b><a href='" + comment.user.html_url + "'>" + comment.user.login + "</a></b>";
+                    t += " posted at ";
+                    t += "<em>" + date.toLocaleString() + "</em>";
+                    t += "<div id='gh-comment-hr'></div>";
+                    // replace emoji with their fallback image because some emoji cannot be displayed.
+                    t += comment.body_html.replace(/fallback-src="([^"]*)"(.*?>).(<\/g-emoji>)/ug, 'fallback-src="$1"$2<img class="emoji" style="vertical-align:middle" height="14" width="14" src="$1">$3');
+                    t += "</div>";
+                    $("#gh-comments-list").append(t);
+                });
 
-          let t = "<div id='gh-comment'>";
-          t += "<img src='" + comment.user.avatar_url + "' width='24px'>";
-          t +=
-            "<b><a href='" +
-            comment.user.html_url +
-            "'>" +
-            comment.user.login +
-            "</a></b>";
-          t += " posted at ";
-          t += "<em>" + date.toUTCString() + "</em>";
-          t += "<div id='gh-comment-hr'></div>";
-          t += comment.body_html;
-          t += "</div>";
-          
-          $("#gh-comments-list").append(t);
+                // Add post button to first page
+                if (page_id == 1)
+                    $("#gh-comments-list").append("<a href='" + url + "#new_comment_field' rel='nofollow' class='btn'>Post a comment on Github</a>");
+
+                // Setup comments button if there are more pages to display
+                var links = ParseLinkHeader(jqXHR.getResponseHeader("Link"));
+                if ("next" in links)
+                {
+                    $("#gh-load-comments").attr("onclick", "DoGithubComments(" + comment_id + "," + (page_id + 1) + ");");
+                    $("#gh-load-comments").show();
+                }
+                else
+                {
+                    $("#gh-load-comments").hide();
+                }
+            },
+            error: function() {
+                $("#gh-comments-list").append("Failed to fetch comments from github, but you can still check them directly <a href='" + url + "'>here</a>.");
+            }
         });
-
-        // Setup comments button if there are more pages to display
-        const links = ParseLinkHeader(jqXHR.getResponseHeader("Link"));
-        if ("next" in links) {
-          $("#gh-load-comments").attr(
-            "onclick",
-            `DoGithubComments(${comment_id},${page_id + 1});`
-          );
-
-          $("#gh-load-comments").show();
-        } else {
-          $("#gh-load-comments").hide();
-        }
-      },
-      error: function() {
-        $("#gh-comments-list").append(
-          "Comments are not open for this post yet."
-        );
-      }
     });
-  });
 }
